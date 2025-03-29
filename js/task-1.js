9b468848bb91752a8e47259c5dc85b54970f4e70
Vitaliia_Rylach <vita.elmura@gmai.com>
2025-03-29 12:15:12 +0100

    task

---
+++ js/task-1.js
@@ -0,0 +1,32 @@
+
+function isEnoughCapacity(products, containerSize) {
+    const value = Object.values(products);  
+    let sum = 0;  
+  
+    for (const item of value) {
+        sum += item;
+    }
+
+   
+    if (sum <= containerSize) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); 
+
+console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);  
+
+console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+);  
+
+console.log(
+    isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); 
